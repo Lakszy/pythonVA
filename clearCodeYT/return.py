9b50a63e46aa078c3983401bbca784a86db9bccf
Lakshay Khattar <lakshaykhattar0208@gmail.com>
2023-12-31 23:52:15 +0530

    fString

--- clearCodeYT/return.py
+++ clearCodeYT/return.py
@@ -1,2 +1,21 @@
 test_var = 'A word'.upper().replace('A','X')
 print(test_var)
+
+
+print(1.1 * 3) #will results in 3.30000000003
+
+num1=10
+num2=20
+
+test_var2 = f"lakshay101 {num1} is a {num2}"
+print(test_var2)
+
+# why we use python
+#Certainly! Here are the advantages of f-strings in concise form:
+
+#1. **Readability:** Clear and concise syntax for embedding expressions.
+#2. **Simplicity:** Intuitive and straightforward compared to older methods.
+#3. **Performance:** Can be more efficient, especially compared to `%` formatting.
+#4. **Flexibility:** Supports a wide range of expressions and formatting options.
+#5. **Integration:** Allows embedding any valid Python expression directly.
+#6. **Consistency:** Promotes a consistent approach to string formatting in Python.
